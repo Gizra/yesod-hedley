139e8745ebd3d8737ce051c90a8c35eafb62baf6
Amitai Burstein <amitai@gizra.com>
2016-01-20 20:31:53 +0200

    Renames

--- Handler/Event.hs
+++ Handler/Event.hs
@@ -3,33 +3,33 @@ module Handler.Event where
 import Import
 
 getEventR :: EventId -> Handler Value
-getEventR pid = do
-    post <- runDB $ get404 pid
+getEventR eid = do
+    event <- runDB $ get404 eid
 
     render <- getUrlRender
-    let renderedUrl = render $ EventR pid
+    let renderedUrl = render $ EventR eid
 
     let links = object
           [ "self" .= renderedUrl
           ]
 
     let returnVal = object
-          [ "data" .= (Entity pid post)
+          [ "data" .= (Entity eid event)
           , "_links" .= links
           ]
 
     return returnVal
 
 putEventR :: EventId -> Handler Value
-putEventR pid = do
-    post <- requireJsonBody :: Handler Event
+putEventR eid = do
+    event <- requireJsonBody :: Handler Event
 
-    runDB $ replace pid post
+    runDB $ replace eid event
 
     sendResponseStatus status204 ()
 
 deleteEventR :: EventId -> Handler Value
-deleteEventR pid = do
-    runDB $ delete pid
+deleteEventR eid = do
+    runDB $ delete eid
 
     sendResponseStatus status204 ()
