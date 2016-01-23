bb763323450b3ade89a809d50cc1c7ce48193434
Amitai Burstein <amitai@gizra.com>
2016-01-23 22:43:00 +0200

    Use Yesod-table

--- Handler/People.hs
+++ Handler/People.hs
@@ -2,10 +2,20 @@ module Handler.People where
 
 import Import
 
+import Yesod.Table (Table)
+import qualified Yesod.Table as Table
+
 getPeopleR :: Handler Html
 getPeopleR = do
     people <- runDB $ selectList [] [ Desc UserId ] :: Handler [Entity User]
 
+    let ppl = map entityVal people
+
     defaultLayout $ do
         setTitle "People List"
-        $(widgetFile "people")
+        Table.buildBootstrap peopleTable ppl
+
+peopleTable :: Table site User
+peopleTable = mempty
+    ++ Table.text   "Username"      userIdent
+    -- ++ Table.text   "Email (todo)"  userPassword
