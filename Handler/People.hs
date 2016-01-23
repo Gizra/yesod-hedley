e02252e3f14d92aaaeefc3108f81013857ec88c0
Amitai Burstein <amitai@gizra.com>
2016-01-23 13:58:24 +0200

    Add People handler

---
+++ Handler/People.hs
@@ -0,0 +1,6 @@
+module Handler.People where
+
+import Import
+
+getPeopleR :: Handler Html
+getPeopleR = error "Not yet implemented: getPeopleR"
---
+++ test/Handler/PeopleSpec.hs
@@ -0,0 +1,10 @@
+module Handler.PeopleSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getPeopleR" $ do
+        error "Spec not implemented: getPeopleR"
+
