162c0b381f2174323ac88437c20a87f2e9c071a4
Amitai Burstein <amitai@gizra.com>
2016-01-25 21:21:43 +0200

    Fix tests

--- test/Handler/EventSpec.hs
+++ test/Handler/EventSpec.hs
@@ -7,7 +7,12 @@ spec = withApp $ do
 
     describe "get single Event" $ do
         it "get single event index" $ do
-            uid <- runDB $ insert $ User "foo" $ Just "bar"
+            uid <- runDB $ insert $ User
+                { userIdent = "foo"
+                , userEmail = "foo@example.com"
+                , userPassword = Nothing
+                , userVerkey = Nothing
+                }
             eid <- runDB $ insert $ Event "title" "body" uid
             get $ EventR eid
             statusIs 200
