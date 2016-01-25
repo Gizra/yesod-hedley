c907f30fe670efbb94d4560027e4d16c84f6ddfa
Amitai Burstein <amitai@gizra.com>
2016-01-25 20:38:26 +0200

    Add UniqueEmail

--- Foundation.hs
+++ Foundation.hs
@@ -143,7 +143,7 @@ instance YesodAuth App where
             Just (Entity uid _) -> return $ Authenticated uid
             Nothing -> Authenticated <$> insert User
                 { userIdent = credsIdent creds
-                , userEmail = credsIdent creds
+                , userEmail = ""
                 , userPassword = Nothing
                 , userVerkey = Nothing
                 }
