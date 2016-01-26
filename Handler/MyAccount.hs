5d2df5be7eeb8dd2f22697f28bc3e84245b24f09
Amitai Burstein <amitai@gizra.com>
2016-01-26 21:54:27 +0200

    Add MyAccountR

---
+++ Handler/MyAccount.hs
@@ -0,0 +1,6 @@
+module Handler.MyAccount where
+
+import Import
+
+getMyAccountR :: Handler Html
+getMyAccountR = error "Not yet implemented: getMyAccountR"
---
+++ test/Handler/MyAccountSpec.hs
@@ -0,0 +1,10 @@
+module Handler.MyAccountSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getMyAccountR" $ do
+        error "Spec not implemented: getMyAccountR"
+
