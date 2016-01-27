7d4a609ee3f348de789d3008d259aea25e7e5e06
Amitai Burstein <amitai@gizra.com>
2016-01-27 23:21:02 +0200

    Re-add where

--- Handler/User.hs
+++ Handler/User.hs
@@ -9,12 +9,11 @@ getUserR :: UserId -> Handler Html
 getUserR userId = do
     user <- runDB $ get404 userId
 
-    let uid = toSqlKey 1 :: UserId -- cast 1 to the expected key
     companies <- runDB
            $ E.select
            $ E.from $ \(company `E.InnerJoin` groupMembership) -> do
                 E.on $ company ^. CompanyUserId E.==. groupMembership ^. GroupMembershipUserId
-                -- E.where_ $ company ^. CompanyUserId E.=. uid -- how to pass ID 1
+                E.where_ $ company ^. CompanyUserId E.==. E.val userId
                 E.limit 5
                 return
                     ( company ^. CompanyId
