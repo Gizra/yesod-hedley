404ec508060720e998b5d4041b6708e33e8f31f1
Amitai Burstein <amitai@gizra.com>
2016-01-27 23:06:31 +0200

    Start fixing query

--- Handler/User.hs
+++ Handler/User.hs
@@ -3,17 +3,18 @@ module Handler.User where
 import Import
 import qualified Database.Esqueleto      as E
 import           Database.Esqueleto      ((^.))
+import Database.Persist.Sql (toSqlKey)
 
 getUserR :: UserId -> Handler Html
 getUserR userId = do
     user <- runDB $ get404 userId
 
-    let authorID = toSqlKey 1 :: AuthorId -- cast 1 to the expected key
+    let uid = toSqlKey 1 :: UserId -- cast 1 to the expected key
     companies <- runDB
            $ E.select
            $ E.from $ \(company `E.InnerJoin` groupMembership) -> do
                 E.on $ company ^. CompanyUserId E.==. groupMembership ^. GroupMembershipUserId
-                E.where_ $ company ^. CompanyUserId E.=. authorID -- how to pass ID 1
+                E.where_ $ company ^. CompanyUserId E.=. uid -- how to pass ID 1
                 E.limit 5
                 return
                     ( company ^. CompanyId
