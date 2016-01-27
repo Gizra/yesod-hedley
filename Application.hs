cb210cd4c6b63556971524f31507a836da5dfbcf
Amitai Burstein <amitai@gizra.com>
2016-01-27 22:29:04 +0200

    Add users page

--- Application.hs
+++ Application.hs
@@ -39,6 +39,7 @@ import Handler.Events
 import Handler.Event
 import Handler.People
 import Handler.MyAccount
+import Handler.User
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/User.hs
@@ -0,0 +1,6 @@
+module Handler.User where
+
+import Import
+
+getUserR :: UserId -> Handler Html
+getUserR userId = error "Not yet implemented: getUserR"
