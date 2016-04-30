c3f9e598b9cfec50e086aa49c64ad60571fc0074
Amitai Burstein <amitai@gizra.com>
2016-04-30 23:34:29 +0300

    Start wiring pager

--- Handler/Events.hs
+++ Handler/Events.hs
@@ -4,20 +4,47 @@ import qualified Data.Text.Read as T
 import Handler.Event
 import Import
 
+
+addPager :: ( PersistEntity val
+            , PersistEntityBackend val ~ YesodPersistBackend m
+            , PersistQuery (YesodPersistBackend m)
+            , Yesod m
+            )
+         => Int
+         -> [ SelectOpt val ]
+         -> HandlerT m IO [ SelectOpt val ]
+addPager resultsPerPage selectOpt  = do
+  mpage <- lookupGetParam "page"
+  let pageNumber = case (T.decimal $ fromMaybe "0" mpage) of
+                      Left _ -> 0
+                      Right (val, _) -> val
+  let pagerOpt = [ LimitTo resultsPerPage
+                 , OffsetBy $ (pageNumber - 1) * resultsPerPage
+                 ]
+  return $ selectOpt `mappend` pagerOpt
+
+
+-- Generalize
+addOrder :: ( Yesod m
+            )
+         => [SelectOpt Event]
+         -> YesodDB m [ SelectOpt Event ]
+addOrder selectOpt = do
+  morder <- lookupGetParam "order"
+  let order = case morder of
+                  Nothing -> Desc EventId
+                  Just val -> Asc EventId
+
+  return $ selectOpt `mappend` [order]
+
+
 getEventsR :: Handler Value
 getEventsR = do
-    let resultsPerPage = 1
-    mpage <- lookupGetParam "page"
-    let pageNumber = case (T.decimal $ fromMaybe "0" mpage) of
-                        Left _ -> 0
-                        Right (val, _) -> val
+    selectOpt <- addPager 5 []
     events <- runDB $
         selectList
             []
-            [ Desc EventId
-            , LimitTo resultsPerPage
-            , OffsetBy $ (pageNumber - 1) * resultsPerPage
-            ] :: Handler [Entity Event]
+            selectOpt :: Handler [Entity Event]
 
     return $ object ["data" .= events]
 
