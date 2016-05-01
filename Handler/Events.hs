65e9bf4ce3eca8f6939e642f13d2347416e9b786
Amitai Burstein <amitai@gizra.com>
2016-05-01 20:24:17 +0300

    Add getCurrentPage

--- Handler/Events.hs
+++ Handler/Events.hs
@@ -7,6 +7,17 @@ import           Handler.Event
 import           Import
 
 
+getCurrentPage :: ( PersistQuery (YesodPersistBackend m)
+                  , Yesod m
+                  )
+               => HandlerT m IO Int
+getCurrentPage = do
+  mpage <- lookupGetParam "page"
+  let pageNumber = case (T.decimal $ fromMaybe "0" mpage) of
+                      Left _ -> 0
+                      Right (val, _) -> val
+  return pageNumber
+
 addPager :: ( PersistEntity val
             , PersistEntityBackend val ~ YesodPersistBackend m
             , PersistQuery (YesodPersistBackend m)
@@ -16,10 +27,8 @@ addPager :: ( PersistEntity val
          -> [ SelectOpt val ]
          -> HandlerT m IO [ SelectOpt val ]
 addPager resultsPerPage selectOpt  = do
-  mpage <- lookupGetParam "page"
-  let pageNumber = case (T.decimal $ fromMaybe "0" mpage) of
-                      Left _ -> 0
-                      Right (val, _) -> val
+  pageNumber <- getCurrentPage
+
   let pagerOpt = [ LimitTo resultsPerPage
                  , OffsetBy $ (pageNumber - 1) * resultsPerPage
                  ]
@@ -56,14 +65,14 @@ addListMetaData keyValues = do
   render <- getUrlRender
   totalCount <- getTotalCount []
 
-  let count = [ "count" .= totalCount ]
-  let selfLink =
+  let metaData =
         [ "self" .= object
             [ "href" .= render EventsR
             , "title" .= String "Self"
             ]
+        , "count" .= totalCount
         ]
-  return $ keyValues `mappend` selfLink `mappend` count
+  return $ keyValues `mappend` metaData
 
 
 orderText2SelectOpt :: [Text] -> [SelectOpt Event]
