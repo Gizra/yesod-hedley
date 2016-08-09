61252156495db18d2b14ed162a8f66d22a03b431
Amitai Burstein <amitai@gizra.com>
2016-08-09 23:17:52 +0300

    Start re-using my-account code

--- Handler/MyAccount.hs
+++ Handler/MyAccount.hs
@@ -1,11 +1,24 @@
 module Handler.MyAccount where
 
 import Import
+import qualified Database.Esqueleto   as E
+import           Database.Esqueleto      ((^.))
 
 getMyAccountR :: Handler Html
 getMyAccountR = do
-    (_, user) <- requireAuthPair
+    (userId, user) <- requireAuthPair
+
+    companies <- runDB
+           . E.select
+           . E.from $ \(company `E.InnerJoin` groupMembership) -> do
+                E.on $ company ^. CompanyId E.==. groupMembership ^. GroupMembershipCompanyId
+                E.where_ $ groupMembership ^. GroupMembershipUserId E.==. E.val userId
+                E.limit 5
+                return
+                    ( company ^. CompanyTitle
+                    , groupMembership ^. GroupMembershipState
+                    )
 
     defaultLayout $ do
-        setTitle "Account"
-        $(widgetFile "my-account")
+        setTitle . toHtml $ userIdent user `mappend` "'s User page"
+        $(widgetFile "user")
