8ed1808416ef1b734a075dd9805838502bf004b2
Amitai Burstein <amitai@gizra.com>
2016-08-10 22:12:00 +0300

    Remove obsolete code

--- Handler/MyAccount.hs
+++ Handler/MyAccount.hs
@@ -1,25 +1,11 @@
 module Handler.MyAccount where
 
 import Import
-import qualified Database.Esqueleto   as E
-import           Database.Esqueleto      ((^.))
 import Utils.Companies
 
 getMyAccountR :: Handler Html
 getMyAccountR = do
     (userId, user) <- requireAuthPair
-
-    companies <- runDB
-           . E.select
-           . E.from $ \(company `E.InnerJoin` groupMembership) -> do
-                E.on $ company ^. CompanyId E.==. groupMembership ^. GroupMembershipCompanyId
-                E.where_ $ groupMembership ^. GroupMembershipUserId E.==. E.val userId
-                E.limit 50
-                return
-                    ( company ^. CompanyTitle
-                    , groupMembership ^. GroupMembershipState
-                    )
-
     companiesWidget <- getUserCompanies userId
 
     defaultLayout $ do
