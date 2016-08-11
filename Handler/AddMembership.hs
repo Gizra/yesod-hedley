aaa88130c59b87a294a2c79c220f0956ae4aedc3
Amitai Burstein <amitai@gizra.com>
2016-08-11 21:10:58 +0300

    Add membership handler

---
+++ Handler/AddMembership.hs
@@ -0,0 +1,9 @@
+module Handler.AddMembership where
+
+import Import
+
+getAddMembershipR :: Handler Html
+getAddMembershipR = error "Not yet implemented: getAddMembershipR"
+
+postAddMembershipR :: Handler Html
+postAddMembershipR = error "Not yet implemented: postAddMembershipR"
---
+++ test/Handler/AddMembershipSpec.hs
@@ -0,0 +1,14 @@
+module Handler.AddMembershipSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getAddMembershipR" $ do
+        error "Spec not implemented: getAddMembershipR"
+
+
+    describe "postAddMembershipR" $ do
+        error "Spec not implemented: postAddMembershipR"
+
