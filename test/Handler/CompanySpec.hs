824a550a9c1cc4832adf4a938f0516801bd16296
Amitai Burstein <amitai@gizra.com>
2016-08-13 14:18:28 +0300

    Add test

--- test/Handler/CompanySpec.hs
+++ test/Handler/CompanySpec.hs
@@ -4,7 +4,16 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
+  describe "get single Company" .
+      it "get single company index" $ do
+          userId <- runDB $ insert User
+              { userIdent = "foo"
+              , userEmail = "foo@example.com"
+              , userPassword = Nothing
+              , userVerkey = Nothing
+              }
 
-    describe "getCompanyR" $ do
-        error "Spec not implemented: getCompanyR"
+          cid <- runDB . insert $ Company "title" userId
 
+          get $ CompanyR cid
+          statusIs 200
